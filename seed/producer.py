5f736b24e66a1755fb27ceeaaad4f82382e52f90
Hugh Evans <hugh.evans.dev@gmail.com>
2026-03-31 12:37:31 +0100

    Kafka free tier to dev tier upgrade

--- seed/producer.py
+++ seed/producer.py
@@ -169,7 +169,7 @@ async def run(settings: Settings) -> None:
         settings.topic_groups_raw,
         settings.topic_events_raw,
         settings.topic_venues_raw,
-    ], num_partitions=2)
+    ], num_partitions=20)
 
     producer = make_producer(settings)
 
